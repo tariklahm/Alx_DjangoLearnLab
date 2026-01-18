a866135ade57ad8d706298f7ee267dc72e24b659
Tarik LAHMAM <tariklahmam91@gmail.com>
2026-01-19 00:24:56 +0100

    Last project

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,6 +12,15 @@ class Author(models.Model):
 class Book(models.Model):
     title=models.CharField(max_length=50)
     author=models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+    
+
     def __str__(self):
         return self.title
     
