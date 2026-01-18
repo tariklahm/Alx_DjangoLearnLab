56a4a91c61d111d1f950504fd4cabdbb9a9a47cc
Tarik LAHMAM <tariklahmam91@gmail.com>
2026-01-18 23:52:24 +0100

    next

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView  # checker expects list_books here
 
-urlpatterns=[
-    path('books/', views.book, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    ]
