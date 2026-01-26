3f5824eb1a1eb833568ddfd7f6dd0b581bde2ecf
Tarik LAHMAM <tariklahmam91@gmail.com>
2026-01-26 11:19:44 +0100

    Adjusl bookshelf model

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,14 +2,91 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-# Create Book model.
+# # Create Book model.
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     publication_year = models.IntegerField()
+# **********************************************************************************
+from django.conf import settings  # <- use this for custom user model
+
+# Author Model
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+# # Book Model
+# class Book(models.Model):
+#     title = models.CharField(max_length=50)
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+#     class Meta:
+#         permissions = [
+#             ("can_add_book", "Can add book"),
+#             ("can_change_book", "Can change book"),
+#             ("can_delete_book", "Can delete book"),
+#         ]
+
+#     def __str__(self):
+#         return self.title
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    title = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
 
 
 
+# Library Model
+class Library(models.Model):
+    name = models.CharField(max_length=50)
+    books = models.ManyToManyField(Book, related_name='libraries')
+
+    def __str__(self):
+        return self.name
+
+
+# Librarian Model
+class Librarian(models.Model):
+    name = models.CharField(max_length=50)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')
+
+    def __str__(self):
+        return self.name
+
+
+# UserProfile Model
+class UserProfile(models.Model):
+    ROLE_CHOICES = (
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    )
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,  # <- updated to custom user model
+        on_delete=models.CASCADE
+    )
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+# *************************************************************************************
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, username, date_of_birth=None, profile_photo=None, password=None, **extra_fields):
