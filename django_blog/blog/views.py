a60c751f4ddee07e4b4cebe5785a0b0925fd7cf8
Tarik LAHMAM <tariklahmam91@gmail.com>
2026-02-15 13:53:32 +0000

    Implementing the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+# Registration view
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log in user after registration
+            messages.success(request, f'Account created for {user.username}!')
+            return redirect('home')  # redirect to home or blog list page
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Profile view
+@login_required
+def profile_view(request):
+    return render(request, 'blog/profile.html')
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]  # if using BASE_DIR as Path object
 
-
+LOGIN_REDIRECT_URL = 'profile'
+LOGOUT_REDIRECT_URL = 'login'
