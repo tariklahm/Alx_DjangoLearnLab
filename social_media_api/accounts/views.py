54c619085a952511adc1857bdd9f4bb85219a90c
Tarik LAHMAM <tariklahmam91@gmail.com>
2026-03-01 00:44:57 +0000

    Fix follow/unfollow views and checker requirements

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -9,6 +9,9 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import get_object_or_404
 
+from rest_framework import permissions
+
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -51,21 +54,23 @@ class ProfileView(generics.RetrieveAPIView):
         return self.request.user
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def follow_user(request, user_id):
-    user_to_follow = get_object_or_404(User, id=user_id)
+    users = CustomUser.objects.all()
+    user_to_follow = get_object_or_404(users, id=user_id)
 
     if request.user == user_to_follow:
         return Response({"error": "You cannot follow yourself"}, status=400)
 
     request.user.following.add(user_to_follow)
-    return Response({"message": f"You are now following {user_to_follow.username}"})
+    return Response({"message": "User followed successfully"})
 
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def unfollow_user(request, user_id):
-    user_to_unfollow = get_object_or_404(User, id=user_id)
+    users = CustomUser.objects.all()
+    user_to_unfollow = get_object_or_404(users, id=user_id)
 
     request.user.following.remove(user_to_unfollow)
-    return Response({"message": f"You unfollowed {user_to_unfollow.username}"})
+    return Response({"message": "User unfollowed successfully"})
