797c63eed403a903e863078c9cf5e6fa57c29513
Tarik LAHMAM <tariklahmam91@gmail.com>
2026-03-01 00:39:14 +0000

    Implementing User Follows and Feed Functionality

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -5,10 +5,11 @@ from django.db import models
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField(
+
+    following = models.ManyToManyField(
         "self",
         symmetrical=False,
-        related_name="following",
+        related_name="followers",
         blank=True
     )
 
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,6 +5,10 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from . serializers import RegisterSerializer, LoginSerializer
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
 
 User = get_user_model()
 
@@ -45,3 +49,23 @@ class ProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(User, id=user_id)
+
+    if request.user == user_to_follow:
+        return Response({"error": "You cannot follow yourself"}, status=400)
+
+    request.user.following.add(user_to_follow)
+    return Response({"message": f"You are now following {user_to_follow.username}"})
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(User, id=user_id)
+
+    request.user.following.remove(user_to_unfollow)
+    return Response({"message": f"You unfollowed {user_to_unfollow.username}"})
